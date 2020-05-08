1a51a4bc7ff587226d5f03c4155f7310103b1550
Aishwarya Bhangale <aishwaryabhangale1997@gmail.com>
2020-05-08 11:03:14 -0400

    Made some changes

--- Machine_Learning_Models/Logistic_Regression_Model.R
+++ Machine_Learning_Models/Logistic_Regression_Model.R
@@ -1,10 +1,10 @@
-setwd("/Users/khanhhuyen4523/Desktop")
+setwd("~/GitHub/Airbnb-New/Machine_Learning_Models")
 set.seed(12345)
 
-df = read.csv("train_cleaned.csv")
+df = read.csv("C:\\Users\\Aishwarya\\Documents\\GitHub\\Airbnb-New\\Data\\train.csv")
 
-df_competition = read.csv("test_cleaned.csv")
-View(df_competition)
+#df_competition = read.csv("test_cleaned.csv")
+#View(df_competition)
 ## Randomly partition the data into 30% testing data and the remaining 70% data.
 test_instn = sample(nrow(df), 0.25*nrow(df))
 df_test <- df[test_instn,]
@@ -16,23 +16,7 @@ df_valid <- df_rest[valid_instn,]
 df_train <- df_rest[-valid_instn,]
 #View(df_competition)
 
-model_log <- glm(high_booking_rate~accommodates+ amenities_count+
-                   
-                   bathrooms+ Real_Bed+ bedrooms+ beds+ price+
-                   cancellation_policy+require_guest_profile_picture+
-                    guests_included+ host_about+host_has_profile_pic+
-                    host_identity_verified+cleaning_fee+
-                   host_is_superhost+
-                   availability_60+availability_90+availability_30+
-                   host_response_rate+ host_response_time+
-                   instant_bookable+ is_business_travel_ready+ 
-                   is_location_exact+ long_stay+
-                    propertyApartment+ propertyCommon_house+
-                   propertySide_house+ propertyHotel+ propertySpecial+
-                   require_guest_phone_verification+ 
-                  requires_license+
-                    roomPrivate.room+
-                   roomShared.room+ security_deposit+ flexible
+model_log <- glm(high_booking_rate~.
                  , data = df_train,family="binomial")
 
 summary(model_log)
@@ -59,23 +43,7 @@ library('caret')
 lambdaValues <- 10^seq(-3, 3, length = 100)
 
 
-fitRidge <- train(as.factor(high_booking_rate)  ~ 
-                    accommodates+ amenities_count+
-                    availability_365+ availability_60+availability_90+availability_30+
-                    bathrooms+ Real_Bed+ bedrooms+ beds+ 
-                    cancellation_policy+ cleaning_fee+ extra_people+
-                    first_review+ guests_included+ host_about+
-                    host_has_profile_pic+ host_identity_verified+
-                    host_is_superhost+ host_listings_count+
-                    host_response_rate+ host_response_time+ experience+
-                    instant_bookable+ is_business_travel_ready+ 
-                    is_location_exact+ long_stay+ minimum_nights+
-                    price+ propertyApartment+ propertyCommon_house+
-                    propertySide_house+ propertyHotel+ propertySpecial+
-                    require_guest_phone_verification+ 
-                    require_guest_profile_picture+ requires_license+
-                    roomEntire.home.apt+ roomPrivate.room+
-                    roomShared.room+ security_deposit+ flexible
+fitRidge <- train(as.factor(high_booking_rate)  ~ .
                   
                   , family='binomial', data=df, method='glmnet', trControl=trainControl(method='cv', number=10), tuneGrid = expand.grid(alpha=0, lambda=lambdaValues))
 
@@ -120,7 +88,7 @@ confusion_matrix <- function(preds, actuals, cutoff){
 }
 
 #log_valid_preds = predict(model_log,newdata=df_valid,type="response")
-log_matrix <- confusion_matrix(log_valid_probs, df_valid$high_booking_rate,0.4804465)
+log_matrix <- confusion_matrix(log_valid_probs, df_valid$high_booking_rate,0.4648672)
 
 acc_log = (log_matrix[1] + log_matrix[4])/sum(log_matrix)
 acc_log
@@ -131,7 +99,7 @@ log_matrix
 
 #Now, fitting that to our test data subsetted from the training data
 log_test_probs <- predict(model_log, newdata = df_test, type = "response")
-log_matrix <- confusion_matrix(log_test_probs, df_test$high_booking_rate,0.4804465)
+log_matrix <- confusion_matrix(log_test_probs, df_test$high_booking_rate,0.4648672)
 acc_log = (log_matrix[1] + log_matrix[4])/sum(log_matrix)
 acc_log
 #0.7848243
--- Machine_Learning_Models/Random Forest Model.R
+++ Machine_Learning_Models/Random Forest Model.R
@@ -1,3 +1,4 @@
+#Importing the required libraries
 library(randomForest)
 require(caTools)
 library(readr)
@@ -6,10 +7,9 @@ library(ROCR)
 set.seed(12345)
 
 
-df <- read_csv("C:\\Users\\Aishwarya\\Documents\\GitHub\\Airbnb-New\\train54.csv")
-names(df)
-#df[,65:96] <- lapply(export_train[,65:96], factor)
-#df[,1:90] <- lapply(df[,1:90], factor)
+df <- read_csv("C:\\Users\\Aishwarya\\Documents\\GitHub\\Airbnb-New\\Data\\train.csv")
+#names(df)
+
 
 
 ## Randomly partition the data into 30% validation data and the remaining 70% data.
@@ -18,56 +18,86 @@ df_valid <- df[test_instn,]
 ## Save the rest of the data as the data that isn't validation
 df_train <- df[-test_instn,]
 
+#Check for any NAs
 which(is.na(df_train))
 
 df_train$high_booking_rate <- as.factor(df_train$high_booking_rate)
 df_valid$high_booking_rate <- as.factor(df_valid$high_booking_rate)
 
-View(df_valid)
-names(df_train)
-df_train$density_10bins<- as.factor(df_train$density_10bins)
-df_valid$density_10bins <- as.factor(df_valid$density_10bins)
-df_train$density_bins<- as.factor(df_train$density_bins)
-df_valid$density_bins <- as.factor(df_valid$density_bins)
+#View(df_valid)
+#names(df_train)
+#df_train$density_10bins<- as.factor(df_train$density_10bins)
+##df_valid$density_10bins <- as.factor(df_valid$density_10bins)
+#df_train$density_bins<- as.factor(df_train$density_bins)
+#df_valid$density_bins <- as.factor(df_valid$density_bins)
 
-names(df_train)
-View(df)
-#c("roomEntire.home.apt", "clustercategory","require_guest_profile_picture","require_guest_phone_verification","host_has_profile_pic", "propertyHotel")
-drop<- c("roomEntire_ home_apt","require_guest_profile_picture","require_guest_phone_verification","host_has_profile_pic",
-         "propertyHotel","weekly_discount","monthly_discount",
-         "weekly_suitable","monthly_suitable","X1")
-drop <-c("X1","X1_1","city_centrality","neighbourhood_restaurant",
-         "maximum_nights","num_listings")#,"roomShared_room","host_has_profile_pic",
-         #"host_identity_verified","host_response_rate",
-         #"propertyHotel","require_guest_phone_verification",
-         #"require_guest_profile_picture","bed_linen",
-         #"coffee_machine","gym","hottub_sauna_pool","long_term_Stay_allowed",
-         #"secure","interaction","cp8","cp10","cp5")
-#Yielded highest accuracy -> 0.839 to 0.8401(this model did not have Bella's
-#and amenities new variables 
-#weekly_suitable, weekly_discount,monthly_suitable, monthly_discount,
-#Family, Check, Stove, Refrigerator, Gym, Elevator, Workspace, Air_conditioning,
-#Parking, Lock, Essentials, Smoking, Cooking, Internet)
+#names(df_train)
+#View(df)
+#,"city_centrality","neighbourhood_restaurant",
+#"maximum_nights","num_listings"
+drop <-c("X1","X1_1")
 
 df_valid <- df_valid[,-which(names(df_valid) %in% drop)]
 df_train <- df_train[,-which(names(df_train) %in% drop)]
 
 sum(is.na(df_train))
 
-names(df_train)
+#names(df_train)
 
 
 #best model, with tuned hyperparameters
 rf<- randomForest(high_booking_rate~.,data = (df_train),
-                  n_estimators = 50, min_sample_leaf = 80, mtry = 20,
+                  n_estimators = 50, min_sample_leaf = 80, mtry = 45,
                   na.rm = TRUE)
 memory.limit(size = 40000000)
+
+#---------------------------------------------------
+#Hyper parameter tuning
+res <- tuneRF(x = subset(df_train,select = - high_booking_rate),
+              y = df_train$high_booking_rate,
+              ntreeTry = 500)
+
+mtree_opt <- res[,"mtry"][which.min(res[,'OOBError'])]
+mtree_opt # 14 (optimal)
+
+
+#tuning for nodesize, sampsize
+mtry <- c(rep(6:6,ncol(df_train)*0.8))
+mtry
+nodesize <-seq(3,8,2)
+sampsize<-nrow(df_train)*c(0.7,0.8)
+
+hyper_grid <- expand.grid(mtry = mtry, nodesize = nodesize, sampsize = sampsize)
+
+oob_err <- c()
+memory.limit(300000000000)
+View(df_train)
+for (i in 1:nrow(hyper_grid)){
+  print(i)
+  model <- randomForest(formula = high_booking_rate~.,
+                        x = df_train[,2:43],
+                        data = df_train,
+                        mtry = mtry[i],
+                        nodesize = nodesize[i],
+                        #sampsize = sampsize[i],
+  )
+  
+  oob_err[i] <- model$err.rate[nrow(model$err.rate),"OOB"]
+}
+
+opt_i <- which.min(oob_err)
+
+print(hyper_grid[opt_i,])
+
+#--------------------------------------------
 names(df_train)
 pred_valid = predict(rf, newdata=df_valid)
 mc = table(df_valid$high_booking_rate, pred_valid)
 mc
 acc = (mc[1] + mc[4])/sum(mc)
 acc
+
+
 
 #----Alternatively:
 library(caret)
@@ -76,7 +106,7 @@ model <- train(
   high_booking_rate~.,
   tuneLength = 1,
   data = df_train, 
-  method = "ranger",
+  method = "ranger"
   #trControl = trainControl(
   #  method = "cv", 
   #  number = , 
